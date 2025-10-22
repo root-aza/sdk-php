be5e75ad4f7f6ea465deb339f06e293bea69ceec
Vlad Shashkov <shashkov.root@gmail.com>
2025-10-22 23:40:14 +0700

    [Testing] Added test stuck child workflow

---
+++ tests/Acceptance/Extra/Workflow/SuppressedExceptionTest.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Acceptance\Extra\Workflow;
+
+use React\Promise\PromiseInterface;
+use Temporal\Client\WorkflowStubInterface;
+use Temporal\DataConverter\Type;
+use Temporal\Interceptor\WorkflowOutboundRequestInterceptor;
+use Temporal\Tests\Acceptance\App\Attribute\Stub;
+use Temporal\Tests\Acceptance\App\TestCase;
+use Temporal\Worker\Transport\Command\RequestInterface;
+use Temporal\Workflow;
+use Temporal\Workflow\ChildWorkflowOptions;
+use Temporal\Workflow\QueryMethod;
+use Temporal\Workflow\ReturnType;
+use Temporal\Workflow\WorkflowInterface;
+use Temporal\Workflow\WorkflowMethod;
+
+final class SuppressedExceptionTest extends TestCase
+{
+    public function childWorkflow(
+        #[Stub('Root_Suppressed_Exception_Workflow')] WorkflowStubInterface $stub,
+    ) {
+        $executedChildWorkflow = false;
+        $deadline              = \microtime(true) + 5.0; // 5-second timeout
+        do {
+            $executedChildWorkflow = $stub->query('isExecutedChildWorkflow')->getValue(0);
+
+            if ($executedChildWorkflow) {
+                break;
+            }
+        } while (\microtime(true) < $deadline);
+
+
+        $this->assertTrue($executedChildWorkflow, 'Child_Suppressed_Exception_Workflow is stuck');
+    }
+}
+
+#[WorkflowInterface]
+final class RootSuppressedExceptionWorkflow
+{
+    public function __construct(
+        private bool $executedChildWorkflow = false,
+    ) {
+    }
+
+    #[WorkflowMethod('Root_Suppressed_Exception_Workflow')]
+    #[ReturnType(Type::TYPE_VOID)]
+    public function start(): \Generator
+    {
+        $childWorkflow = Workflow::newChildWorkflowStub(
+            ChildSuppressedExceptionWorkflow::class,
+            ChildWorkflowOptions::new()
+                ->withSearchAttributes([
+                    'SuppressedException' => true,
+                ])
+        );
+
+        yield $childWorkflow->start();
+
+        $this->executedChildWorkflow = true;
+    }
+
+    #[QueryMethod]
+    public function isExecutedChildWorkflow(): bool
+    {
+        return $this->executedChildWorkflow;
+    }
+}
+
+
+
+#[WorkflowInterface]
+final class ChildSuppressedExceptionWorkflow
+{
+    #[WorkflowMethod('Child_Suppressed_Exception_Workflow')]
+    public function start(): \Generator
+    {
+        yield Workflow::getVersion('SUPPRESSED_EXCEPTION', 1, 1);
+    }
+}
+
+
+final class SuppressedExceptionWorkflowOutboundRequestInterceptor implements WorkflowOutboundRequestInterceptor
+{
+    public function handleOutboundRequest(RequestInterface $request, callable $next): PromiseInterface
+    {
+        /** @var bool|null $attribute */
+        $attribute = Workflow::getInfo()->searchAttributes['SuppressedException'] ?? null;
+
+        if ($attribute) {
+            $headers = $request->getHeader()
+                ->withValue('suppressedException', $attribute)
+            ;
+
+            $request = $request->withHeader($headers);
+        }
+
+        return $next($request);
+    }
+}
